d09f594b5fec3332e64e476cbdc1f7af03525793
Tran Ngoc Dang Khoa <tndangkhoa218@gmail.com>
2025-01-25 18:12:29 +0700

    chore: setup (#1)

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,21 @@
 import { NestFactory } from '@nestjs/core';
+
 import { AppModule } from './app.module';
 
-async function bootstrap() {
+async function bootstrap(): Promise<void> {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  console.log(process.env.PORT);
+  await app.listen(parseInt(process.env.PORT, 10));
 }
-bootstrap();
+
+(async (): Promise<void> => {
+  try {
+    await bootstrap();
+
+    if (process.send) {
+      process.send('ready');
+    }
+  } catch (error) {
+    console.error(error, 'Bootstrap');
+  }
+})();
