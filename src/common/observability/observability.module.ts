2ae35df12d9ed834e06eb1e53386c0bcae364f8e
Jikei <khangphamcs@gmail.com>
2025-03-22 14:19:31 +0700

    chore: setup metrics

--- src/common/observability/observability.module.ts
+++ src/common/observability/observability.module.ts
@@ -1,18 +1,28 @@
 import { Module } from '@nestjs/common';
+import { APP_INTERCEPTOR } from '@nestjs/core';
 import { TerminusModule } from '@nestjs/terminus';
 import { PrometheusModule } from '@willsoto/nestjs-prometheus';
 
 import { HealthController, MetricsController } from './controllers';
+import { MetricsInterceptor } from './interceptors/metrics.interceptor';
 import { metricsProviders } from './providers';
 import { MetricsService } from './services';
 
 @Module({
   imports: [
     TerminusModule,
-    PrometheusModule.register({ controller: MetricsController }),
+    PrometheusModule.register({
+      controller: MetricsController,
+    }),
   ],
   controllers: [HealthController],
-  providers: [MetricsService, ...metricsProviders],
-  exports: [MetricsService],
+  providers: [
+    MetricsService,
+    {
+      provide: APP_INTERCEPTOR,
+      useClass: MetricsInterceptor,
+    },
+    ...metricsProviders,
+  ],
 })
 export class ObservabilityModule {}
