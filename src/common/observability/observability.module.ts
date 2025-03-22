b6c20faaa8c93f43a6909ea12e01c11f828ead6c
Jikei <khangphamcs@gmail.com>
2025-03-22 15:43:36 +0700

    fix: fix convention naming

--- src/common/observability/observability.module.ts
+++ src/common/observability/observability.module.ts
@@ -1,28 +1,18 @@
 import { Module } from '@nestjs/common';
-import { APP_INTERCEPTOR } from '@nestjs/core';
 import { TerminusModule } from '@nestjs/terminus';
 import { PrometheusModule } from '@willsoto/nestjs-prometheus';
 
 import { HealthController, MetricsController } from './controllers';
-import { MetricsInterceptor } from './interceptors/metrics.interceptor';
 import { metricsProviders } from './providers';
 import { MetricsService } from './services';
 
 @Module({
   imports: [
     TerminusModule,
-    PrometheusModule.register({
-      controller: MetricsController,
-    }),
+    PrometheusModule.register({ controller: MetricsController }),
   ],
   controllers: [HealthController],
-  providers: [
-    MetricsService,
-    {
-      provide: APP_INTERCEPTOR,
-      useClass: MetricsInterceptor,
-    },
-    ...metricsProviders,
-  ],
+  providers: [MetricsService, ...metricsProviders],
+  exports: [MetricsService],
 })
 export class ObservabilityModule {}
