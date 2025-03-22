9e011e22fe1afa62c4a23053a38ad1649a095da5
Jikei <khangphamcs@gmail.com>
2025-03-22 15:12:38 +0700

    chore: change status code format

--- src/common/observability/services/metrics.service.ts
+++ src/common/observability/services/metrics.service.ts
@@ -4,6 +4,15 @@ import { Counter, Histogram, Summary } from 'prom-client';
 
 import { MetricsDto } from '../dtos';
 
+// eslint-disable-next-line @typescript-eslint/naming-convention
+enum STATUS_CODE_TYPE {
+  CLIENT_ERROR = '4xx',
+  SERVER_ERROR = '5xx',
+  SUCCESS = '2xx',
+  REDIRECT = '3xx',
+  INFORMATION = '1xx',
+}
+
 @Injectable()
 export class MetricsService {
   constructor(
@@ -28,10 +37,17 @@ export class MetricsService {
 
     this.httpRequestDurationSummary.observe({ method, route }, duration);
 
-    this.httpStatusCodesCounter.inc({
-      method,
-      route,
-      status_code: statusCode.toString(),
-    });
+    let statusType = STATUS_CODE_TYPE.INFORMATION;
+    if (statusCode >= 500) {
+      statusType = STATUS_CODE_TYPE.SERVER_ERROR;
+    } else if (statusCode >= 400) {
+      statusType = STATUS_CODE_TYPE.CLIENT_ERROR;
+    } else if (statusCode >= 300) {
+      statusType = STATUS_CODE_TYPE.REDIRECT;
+    } else if (statusCode >= 200) {
+      statusType = STATUS_CODE_TYPE.SUCCESS;
+    }
+
+    this.httpStatusCodesCounter.inc({ method, route, status_code: statusType });
   }
 }
